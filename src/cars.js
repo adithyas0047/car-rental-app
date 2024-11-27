9ff340f40c59548686aba18b967e8d7ac0cf1b8e
Adithya S <102845742+adithyas0047@users.noreply.github.com>
2024-11-27 19:14:50 +0530

    Add files via upload

---
+++ src/cars.js
@@ -0,0 +1,3 @@
+const cars = [
+    { id: 1, name: "Toyota Corolla", price: "$50/day", image: "toyota.jpg" },
+    { id: 2, n
---
+++ src/navbar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => (
+  <nav className="navbar">
+    <h1>Car Rental</h1>
+    <div>
+  
